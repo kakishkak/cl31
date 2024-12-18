dcba6e68809215a74cac73063916588a1ec843a5
kakishkak <kolya.yokimavich@gmail.com>
2024-12-18 19:19:21 +0200

    Update cl31.cpp

--- cl31/cl31.cpp
+++ cl31/cl31.cpp
@@ -2,59 +2,129 @@
 
 using namespace std;
 
-class Mobile {
-public:
-    virtual bool HaveButton() = 0;
-    virtual bool CanCall() = 0;
-    virtual void TikToks() {
-        cout << "Cant record";
-    }
-    virtual void Playing() {
-        cout << "cant play";
-    }
+//class Mobile {
+//public:
+//    virtual bool HaveButton() = 0;
+//    virtual bool CanCall() = 0;
+//    virtual void TikToks() {
+//        cout << "Cant record";
+//    }
+//    virtual void Playing() {
+//        cout << "cant play";
+//    }
+//
+//};
+//
+//class Smartphone : public Mobile {
+//private:
+//    bool HaveSensorScreen;
+//public:
+//
+//    void Playing() override {
+//        cout << "This phone for gaming" << endl;
+//    }
+//    bool HaveButton() override {
+//        return true;
+//    }
+//    bool CanCall() override {
+//        return true;
+//    }
+//};
+//
+//class Cameraphone : public Mobile {
+//private:
+//    bool HaveCoolCAmera;
+//public:
+//
+//    void TikToks() override {
+//        cout << "This phone can record" << endl;
+//    }
+//    bool HaveButton() override {
+//        return true;
+//    }
+//    bool CanCall() override {
+//        return true;
+//    }
+//};
 
+//int main()
+
+    //Mobile* obj = new Cameraphone();
+    //
+    //obj->TikToks();
+    //
+    //obj = new Smartphone();
+    //
+    //obj->Playing();
+
+
+
+#include <iostream>
+
+using namespace std;
+
+class IMovable {
+public:
+    virtual void Move() = 0;
 };
 
-class Smartphone : public Mobile {
-private:
-    bool HaveSensorScreen;
+class IDrivable {
 public:
+    virtual void Drive() = 0;
+};
 
-    void Playing() override {
-        cout << "This phone for gaming" << endl;
-    }
-    bool HaveButton() override {
-        return true;
-    }
-    bool CanCall() override {
-        return true;
+class IFillable {
+public:
+    virtual void Fill() = 0;
+};
+
+class IFlyable {
+public:
+    virtual void Fly() = 0;
+};
+
+
+
+class Car : public IDrivable {
+public:
+    void Drive() override {
+        cout << "Car can move" << endl;
     }
 };
 
-class Cameraphone : public Mobile {
-private:
-    bool HaveCoolCAmera;
+class Human : public IMovable {
 public:
+    void Move() override {
+        cout << "Person can move" << endl;
+    }
+};
 
-    void TikToks() override {
-        cout << "This phone can record" << endl;
+class Flying : public IFlyable {
+public:
+    void Fly() override {
+        cout << "It can fly" << endl;
     }
-    bool HaveButton() override {
-        return true;
-    }
-    bool CanCall() override {
-        return true;
+};
+
+class Ship : public IFillable {
+public:
+    void Fill() override {
+        cout << "Ship can walk" << endl;
     }
 };
 
 int main()
 {
-    Mobile* obj = new Cameraphone();
 
-    obj->TikToks();
+    Car objCar = Car();
+    Human objHuman = Human();
+    Flying objFlying = Flying();
+    Ship objShip = Ship();
 
-    obj = new Smartphone();
+    objCar.Drive();
+    objHuman.Move();
+    objFlying.Fly();
+    objShip.Fill();
 
-    obj->Playing();
 }
 
