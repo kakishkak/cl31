63a8c6daa162145232f757e24337ac1b12716022
kakishkak <kolya.yokimavich@gmail.com>
2024-12-18 18:44:21 +0200

    Update cl31.cpp

--- cl31/cl31.cpp
+++ cl31/cl31.cpp
@@ -2,7 +2,59 @@
 
 using namespace std;
 
+class Mobile {
+public:
+    virtual bool HaveButton() = 0;
+    virtual bool CanCall() = 0;
+    virtual void TikToks() {
+        cout << "Cant record";
+    }
+    virtual void Playing() {
+        cout << "cant play";
+    }
+
+};
+
+class Smartphone : public Mobile {
+private:
+    bool HaveSensorScreen;
+public:
+
+    void Playing() override {
+        cout << "This phone for gaming" << endl;
+    }
+    bool HaveButton() override {
+        return true;
+    }
+    bool CanCall() override {
+        return true;
+    }
+};
+
+class Cameraphone : public Mobile {
+private:
+    bool HaveCoolCAmera;
+public:
+
+    void TikToks() override {
+        cout << "This phone can record" << endl;
+    }
+    bool HaveButton() override {
+        return true;
+    }
+    bool CanCall() override {
+        return true;
+    }
+};
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    Mobile* obj = new Cameraphone();
+
+    obj->TikToks();
+
+    obj = new Smartphone();
+
+    obj->Playing();
 }
+
